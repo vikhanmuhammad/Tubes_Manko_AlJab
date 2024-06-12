6a00455cb0710668e794feefac1136e744f4864f
Vikhan Muharram <127295441+vikhanmuhammad@users.noreply.github.com>
2024-06-12 21:11:35 +0700

    Update AppTest.java

--- src/test/java/uas/AppTest.java
+++ src/test/java/uas/AppTest.java
@@ -3,6 +3,7 @@ package uas;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
